6d05a4faa990e0260a2ac72351724e156303a141
cb2hofmann <32701541+cb2hofmann@users.noreply.github.com>
2017-10-19 09:39:57 +0200

    Einlesen von Json-Datei

---
+++ twitterdaten_einlesenjson1.R
@@ -0,0 +1,24 @@
+library(jsonlite)
+library(rjson)
+# winners <- fromJSON("00.json")
+# colnames(winners)
+
+
+# read in individual JSON lines
+json_str <-"C:/Users/Christian/Documents/textmining/00.json"
+#json_str <-"C:/Users/Christian/Documents/textmining/prob.json"
+a<-fromJSON(json_str, nullValue = NA)
+#asFrame <- do.call("rbind.fill", lapply(a, as.data.frame))
+#df <- lapply(a, function(play) # Loop through each "play"
+{
+  # Convert each group to a data frame.
+  # This assumes you have 6 elements each time
+  data.frame(matrix(unlist(play), ncol=1, byrow=T))
+})
+
+#df<-data.frame(matrix(unlist(a), ncol=1, byrow=T))
+#df <- do.call(rbind, df)
+
+# Make column names nicer, remove row names
+#colnames(df) <- names(my.JSON[[1]][[1]])
+#rownames(df) <- NULL

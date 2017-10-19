e1debac670ba39fcbfd988163ff532e7a700418d
cb2hofmann <32701541+cb2hofmann@users.noreply.github.com>
2017-10-19 09:44:50 +0200

    json datei einlesen als txt-datei

---
+++ jsoneinlesen_als_text.R
@@ -0,0 +1,7 @@
+library(rjson)
+Lines <- readLines("C:/Users/Christian/Documents/textmining/00.json") 
+#business <- as.data.frame(t(sapply(Lines, fromJSON)))
+a<-sapply(c("C:/Users/Christian/Documents/textmining/Daten/d/00.json","C:/Users/Christian/Documents/textmining/Daten/d/01.json"),readLines)
+
+p<-readLines(df)
+readL

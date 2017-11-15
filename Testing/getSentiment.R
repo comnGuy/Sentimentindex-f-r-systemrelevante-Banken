b5026b0a85cce2ad280533e41f352fee39808408
Bernhard Preisler <Bernhard.Preisler.ext@devoteam.com>
2017-11-15 11:16:42 +0100

    Complete

--- Testing/getSentiment.R
+++ Testing/getSentiment.R
@@ -1,9 +1,11 @@
 
 require(ggplot2)
 library(tidyr)
+library(tidytext)
+library(dplyr)
 
+setwd('C:/Clouds/OwnCloud/Studium/Text Mining/Sentimentindex-f-r-systemrelevante-Banken/Testing')
 normalDataFrame <- read.csv("data/splittedDataframe1.csv")
-
 
 
 
@@ -38,15 +40,27 @@ tidy_books <- normalDataFrame %>%
 
 
 sentiments <- tidy_books %>%
-  inner_join(get_sentiments("bing")) %>%
+  inner_join(get_sentiments("loughran")) %>%
   count(X, index = linenumber %/% 80, sentiment) %>%
   spread(sentiment, n, fill = 0) %>%
   mutate(sentiment = positive - negative)
 
 
-
+summary(normalDataFrame$Min)
 
 sum(sentiments$negative)
 sum(sentiments$positive)
 
 head(sentiments,20)
+
+
+
+total <- merge(normalDataFrame, sentiments,by="X")
+
+summary(total$DayOfMonth)
+
+
+ggplot(total, aes(X, sentiment, fill = Follower)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~DayOfMonth, ncol = 2, scales = "free_x")
+

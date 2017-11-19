0d05c747db6d2727ab2ce1a6c70034b6c8ec3e9f
Christian Hofmann <Christian.Hofmann.ext@devoteam.com>
2017-11-19 19:43:03 +0100

    neu

--- Testing/test_sent2.R
+++ Testing/test_sent2.R
@@ -3,6 +3,8 @@ library(tidyr)
 library(tidytext)
 library(dplyr)
 library(lubridate)
+library(reshape2)
+library(wordcloud)
 #usa pro Monat
 daten_usa<-read.csv("C:/Users/Christian/Documents/textmining/R-projekt/BeckerSeminar2/Testing/Daten2012usa.csv")
 data_fr_usa<- data.frame(daten_usa, stringsAsFactors=FALSE)
@@ -45,8 +47,56 @@ dif_us<-tidy_2012_ohne_stopwords  %>%
 
 ggplot(data=dif_us, aes(x=Month2, y=sentiment),fill=sentiment) + geom_col(show.legend = FALSE)+
   geom_bar(stat="identity")
+#wordcloud usa-------------------------------------------
+word_cloud_usa<-tidy_2012_ohne_stopwords  %>%
+  inner_join(bing) %>%
+count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("#F8766D", "#00BFC4"),
+                   max.words = 100)
+#Die Ranking postiv und negative Wörts USA---------------------------------------------------------------------------------
+wordcount <-tidy_2012_ohne_stopwords %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
 
-#Griechenland pro Monat
+wordcount %>%
+  group_by(sentiment) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(y = "Contribution to sentiment",
+       x = NULL) +
+  coord_flip()
+#Vergleich Nrc, Bing und AFINN USA----------------------------------------------------------------------------------
+
+afinn <- tidy_2012_ohne_stopwords%>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(Month2) %>% 
+  summarise(sentiment = sum(score)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc <- bind_rows(tidy_2012_ohne_stopwords%>% 
+                            inner_join(get_sentiments("bing")) %>%
+                            mutate(method = "Bing et al."),
+                          tidy_2012_ohne_stopwords %>% 
+                            inner_join(get_sentiments("nrc") %>% 
+                                         filter(sentiment %in% c("positive","negative"))) %>%
+                            mutate(method = "NRC")) %>%
+  count(method, Month2, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn, 
+          bing_and_nrc) %>%
+  ggplot(aes(Month2, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
+
+#Griechenland pro Monat-----------------------------------------------------------
 daten_griechenland<-read.csv("C:/Users/Christian/Documents/textmining/R-projekt/BeckerSeminar2/Testing/Daten2012europaneugrie.csv")
 data_fr_griechenland<- data.frame(daten_griechenland)
 data_fr_griechenland$Tweets<-as.character(data_fr_griechenland$Tweets)
@@ -76,7 +126,7 @@ datplot_gr<-tidy_2012_ohne_stopwords_gr  %>%
 ggplot(data=datplot_gr, aes(x=Month2, y=n, fill=sentiment)) + geom_col(show.legend = FALSE)+
   geom_bar(stat="identity") + facet_wrap(~sentiment, ncol = 2, scales = "free_x")
 
-#differrenz
+#differrenz----------------------------------------------------------------------------------------------
 dif_griechen<-tidy_2012_ohne_stopwords_gr  %>%
   inner_join(bing) %>%
   group_by(Month2)%>%
@@ -87,5 +137,53 @@ dif_griechen<-tidy_2012_ohne_stopwords_gr  %>%
 ggplot(data=dif_griechen, aes(x=Month2, y=sentiment),fill=sentiment) + geom_col(show.legend = FALSE)+
   geom_bar(stat="identity")
 
+#wordcloud griechenland-------------------------------------------
+word_cloud_usa<-tidy_2012_ohne_stopwords_gr  %>%
+  inner_join(bing) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+  comparison.cloud(colors = c("#F8766D", "#00BFC4"),
+                   max.words = 100)
+#Die Ranking postiv und negative Wörts Griechenland---------------------------------------------------------------------------------
+wordcount <-tidy_2012_ohne_stopwords_gr %>%
+inner_join(get_sentiments("bing")) %>%
+  count(word, sentiment, sort = TRUE) %>%
+  ungroup()
+
+wordcount %>%
+  group_by(sentiment) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(y = "Contribution to sentiment",
+       x = NULL) +
+  coord_flip()
+#Vergleich Nrc, Bing und AFINN Griechenland----------------------------------------------------------------------------------
+
+afinn_gr <- tidy_2012_ohne_stopwords_gr%>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(Month2) %>% 
+  summarise(sentiment = sum(score)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc_gr <- bind_rows(tidy_2012_ohne_stopwords_gr%>% 
+                            inner_join(get_sentiments("bing")) %>%
+                            mutate(method = "Bing et al."),
+                          tidy_2012_ohne_stopwords_gr %>% 
+                            inner_join(get_sentiments("nrc") %>% 
+                                         filter(sentiment %in% c("positive","negative"))) %>%
+                            mutate(method = "NRC")) %>%
+  count(method, Month2, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn_gr, 
+          bing_and_nrc_gr) %>%
+  ggplot(aes(Month2, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
 class(tidy_daten2012_word$Month)
 

6d3dd96dd405c69d7fb0d1e63b18b98dc825b86c
Bernhard Preisler <Bernhard.Preisler.ext@devoteam.com>
2017-12-07 09:52:27 +0100

    A function to generate a data frame for regression

---
+++ Testing/functions.R
@@ -0,0 +1,24 @@
+
+
+generateLinearRegressionDataFrame <- function(dataframe, columnSizeSentiment = 3, prediction = 0, sentimentColumnName = 'sentiment', renditeColumnName = 'Rendite', renditeColumnNameOutput = 'rendite'){
+  tmpFrame <- data.frame(matrix(NA, nrow = 1, ncol = columnSizeSentiment + 1))
+  colnames(tmpFrame) <- c(1:columnSizeSentiment, renditeColumnNameOutput)
+  
+  columnSizeSentiment <- columnSizeSentiment - 1
+  countRow <- 1
+  for (i in 1:nrow(dataframe)) {
+    for(j in 0:columnSizeSentiment) {
+      tmpFrame[countRow, j+1] <- dataframe[i+j, sentimentColumnName]
+    }
+    tmpFrame[countRow, renditeColumnNameOutput] <- dataframe[i + columnSizeSentiment + prediction, renditeColumnName]
+    countRow <- countRow + 1
+  }
+  tmpFrame[complete.cases(tmpFrame), ]
+}
+
+generateLinearRegressionDataFrame(rendite, 
+                                  columnSizeSentiment = 3,
+                                  prediction = 1,
+                                  sentimentColumnName = 'sentiment',
+                                  renditeColumnName = 'Rendite',
+                                  renditeColumnNameOutput = 'rendite')

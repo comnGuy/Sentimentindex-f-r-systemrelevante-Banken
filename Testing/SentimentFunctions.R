4f28a6ea206fb0736fd60f1b09dd039a0ce78e39
Christian Hofmann <Christian.Hofmann.ext@devoteam.com>
2018-01-20 12:32:16 +0100

    update

--- Testing/SentimentFunctions.R
+++ Testing/SentimentFunctions.R
@@ -1,37 +1,52 @@
 
 # sentimentTest
 # You should give a data frame with a 
-compareSentiments <- function(sentimentTest, sentimentOriginal) {
+compareSentiments <- function(sentimentTest, sentimentOriginal, sentimentBook) {
   sentimentTest$sentiment[sentimentTest$sentiment < 0] <- -1
   sentimentTest$sentiment[sentimentTest$sentiment > 0] <- 1
   
   mergedSentiments <- merge(sentimentTest, sentimentOriginal, by = 'X')
   
-  generelHits <- count(mergedSentiments[mergedSentiments$sentiment == mergedSentiments$sentimentOriginal,])
+  Hits <- count(mergedSentiments[mergedSentiments$sentiment == mergedSentiments$sentimentOriginal,])
+  noHits <- count(sentimentOriginal) - Hits
   positive <- count(mergedSentiments[mergedSentiments$sentiment == mergedSentiments$sentimentOriginal & mergedSentiments$sentimentOriginal == 1,])
   negative <- count(mergedSentiments[mergedSentiments$sentiment == mergedSentiments$sentimentOriginal & mergedSentiments$sentimentOriginal == -1,])
-  noHits <- count(sentimentOriginal) - generelHits
+  neutral <- count(mergedSentiments[mergedSentiments$sentiment == mergedSentiments$sentimentOriginal & mergedSentiments$sentimentOriginal == 0,])
   
-  results <- c(positive$n, negative$n, generelHits$n, noHits$n)
-  names <- c("positive", "negative", "generelHits", "noHits")
-  data.frame(results, names)
+  results <- c(positive$n, negative$n, neutral$n, Hits$n, noHits$n)
+  names <- c("Positive", "Negative", "Neutral", "Hits", "noHits")
+  sentimentBookC <- c(sentimentBook, sentimentBook, sentimentBook, sentimentBook, sentimentBook)
+  data.frame(results, names, sentimentBookC)
 }
+
+
 
 #
 #
-generateSentiment <- function(dataFrame, columnText = 'full_text', ColumnWords = 'word') {
+generateSentiment <- function(dataFrame, columnText = 'full_text', ColumnWords = 'word', sentimentBook = 'afinn') {
   # Set the column as char
   dataFrame[[columnText]] <- as.character(dataFrame[[columnText]])
   
   # Split up the Tweets
-  tidy_books <- normalDataFrame %>%
+  tidy_books <- dataFrame %>%
     mutate(X = row_number()) %>%
     ungroup() %>%
     unnest_tokens_(ColumnWords, columnText)
   
+  sentiments <- ''
   # Summarize the word by Tweet
-  sentiments <- tidy_books %>%
-    inner_join(get_sentiments("afinn"), by = ColumnWords) %>%
-    group_by(X) %>%
-    summarise(sentiment = sum(score))
+  if(sentimentBook == 'afinn') {
+    sentiments <- tidy_books %>%
+      inner_join(get_sentiments(sentimentBook), by = ColumnWords) %>%
+      group_by(X) %>%
+      summarise(sentiment = sum(score))
+  }
+  if(sentimentBook == 'bing' || sentimentBook == 'loughran') {
+    sentiments <- tidy_books %>%
+      inner_join(get_sentiments(sentimentBook)) %>%
+      count(X, index = X %/% 80, sentiment) %>%
+      spread(sentiment, n, fill = 0) %>%
+      mutate(sentiment = positive - negative)
+  }
+  sentiments
 }

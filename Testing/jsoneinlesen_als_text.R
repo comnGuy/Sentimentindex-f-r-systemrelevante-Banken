025d29e90f834bfe5d8882fe1eb94fd338d4f53c
Bernhard Preisler <Bernhard.Preisler.ext@devoteam.com>
2018-01-31 11:22:56 +0100

    Dateien verschoben Anhang erweitert (Dateien beschrieben)

--- Testing/jsoneinlesen_als_text.R
+++
@@ -1,5 +0,0 @@
-library(rjson)
-Lines <- readLines("C:/Users/Christian/Documents/textmining/00.json") 
-
-a<-sapply(c("C:/Users/Christian/Documents/textmining/Daten/d/00.json","C:/Users/Christian/Documents/textmining/Daten/d/01.json"),readLines)
-
